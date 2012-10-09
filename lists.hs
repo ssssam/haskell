14ffeb0c315963be7a84512392c73ce43c54c62c
Sam Thursfield <sam@afuera.me.uk>
2012-10-09 18:52:07 +0100

    More lists

--- lists.hs
+++ lists.hs
@@ -18,3 +18,37 @@ toList Nil = []
 data Tree a = Tree (Maybe (Tree a)) (Maybe (Tree a))
               deriving (Show)
 
+-- Exercise 1
+
+myLength :: [a] -> Int
+
+myLength (x:xs) = (myLength xs) + 1
+myLength [] = 0
+
+-- Exercise 3
+
+-- No polymorphic type to allow finding mean of list of Int?
+
+mean :: [Double] -> Double
+mean x = (sum x) / (fromIntegral (length x))
+
+-- Exercise 4
+
+palindrome :: [a] -> [a]
+palindrome x = x ++ (reverse x)
+
+-- Exercise 5
+
+
+dropLast :: [a] -> [a]
+dropLast x = (reverse (tail (reverse (x))))
+
+-- isPalindrome :: [a] -> Bool
+isPalindrome :: [Int] -> Bool
+isPalindrome [] = True
+isPalindrome x | length x == 1 = True
+isPalindrome (x:xs) = if x /= (last xs)
+                      then False
+                      else (isPalindrome (dropLast xs))
+
+
