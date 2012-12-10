42cfbe6aa2b64e1bf2290a2e302ffdf2adadc983
Sam Thursfield <sam@afuera.me.uk>
2012-12-10 23:43:17 +0000

    Add initial version of lastfm history parser
    With a large DB the current code takes about 15 seconds to lookup an
    MBID and uses 512MB of RAM in the process.
    
    I presume the simplest fix is to use ByteString.Lazy to read the file.

---
+++ lastfm/main.hs
@@ -0,0 +1,58 @@
+import qualified Data.Map
+import System.Environment
+
+parseArgs :: [String] -> String
+parseArgs (filename:[]) = filename
+parseArgs _ = error "Expected: path to dumpfile"
+
+-- attempt #1 at writing split from scratch -- still a lot to learn!
+split :: (a -> Bool) -> [a] -> [[a]]
+split predicate (x:xs) =
+        let rest = (split predicate xs)
+        in if predicate x then
+            ([] : rest)
+        else
+            ([x] ++ (head rest)) : (tail rest)
+split predicate x = [x]
+
+data Listen = Listen {
+    timestamp :: Int,
+    recordingName :: String,
+    artistName :: String,
+    albumName :: String,
+    trackMBID :: String,
+    artistMBID :: String,
+    albumMBID :: String
+} deriving (Show)
+
+parseListen :: [String] -> Listen
+parseListen (p1:p2:p3:p4:p5:p6:p7:[]) =
+    Listen (read p1 :: Int) p2 p3 p4 p5 p6 p7
+parseListen _ = error "Parse error"
+
+parseDumpLine :: String -> Listen
+parseDumpLine text = (parseListen (split ((==) '\t') text))
+
+mapIdToArtist :: [Listen] -> Data.Map.Map String String
+mapIdToArtist listens = foldr step (Data.Map.empty) listens
+    where
+        step listen map
+            | (artistMBID listen) /= "" = 
+                (Data.Map.insert (artistMBID listen) (artistName listen) map)
+            | otherwise = map
+          
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let dumpfilename = parseArgs args
+    contents <- readFile dumpfilename
+    let listens = map parseDumpLine (lines contents)
+    let idToArtist = (mapIdToArtist listens)
+    print (Data.Map.lookup "e09983cb-182d-4e45-b085-664800953cf1" idToArtist)
+    print (Data.Map.lookup "9975369b-6fc1-455e-91d4-d3c8b1772e57" idToArtist)
+    -- readFile dump
+    putStrLn "Done"
+
+-- Things to actually do with this code
+-- Index by artist
